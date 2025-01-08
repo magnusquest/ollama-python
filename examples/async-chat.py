d0c1609fa56d6478fbd5f9087f88a2b89e5e587b
magnusquest <thisismartinc@gmail.com>
2025-01-07 20:42:57 -0500

    working

--- examples/async-chat.py
+++ examples/async-chat.py
@@ -3,17 +3,17 @@ from ollama import AsyncClient
 
 
 async def main():
-  messages = [
-    {
-      'role': 'user',
-      'content': 'Why is the sky blue?',
-    },
-  ]
+    messages = [
+        {
+            "role": "user",
+            "content": "Why is the sky blue?",
+        },
+    ]
 
-  client = AsyncClient()
-  response = await client.chat('llama3.2', messages=messages)
-  print(response['message']['content'])
+    client = AsyncClient()
+    response = await client.chat("llama3.2:3b", messages=messages)
+    print(response["message"]["content"])
 
 
-if __name__ == '__main__':
-  asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
--- examples/fill-in-middle.py
+++ examples/fill-in-middle.py
@@ -1,22 +1,20 @@
 from ollama import generate
-
+# hi
 prompt = '''def remove_non_ascii(s: str) -> str:
     """ '''
 
-suffix = """
-    return result
-"""
+suffix = "return result"
 
 response = generate(
-  model='codellama:7b-code',
-  prompt=prompt,
-  suffix=suffix,
-  options={
-    'num_predict': 128,
-    'temperature': 0,
-    'top_p': 0.9,
-    'stop': ['<EOT>'],
-  },
+    model="qwen2.5-coder:14b",
+    prompt=prompt,
+    suffix=suffix,
+    options={
+        "num_predict": 128,
+        "temperature": 0,
+        "top_p": 0.9,
+        "stop": ["<EOT>"],
+    },
 )
 
-print(response['response'])
+print(response["response"])
